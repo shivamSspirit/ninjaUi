4be3b0a8adc1698c024d40cb6b7d571f3e2d10d4
shivam <shivam.spirite3@outlook.com>
2022-05-29 21:48:24 +0530

    refaormatting files

--- Components/badges/badge.js
+++ Components/badges/badge.js
@@ -1,7 +1,5 @@
 const statusBadge = document.querySelectorAll(".status-number")
 console.log('status',statusBadge[0].innerHTML);
-
- 
      let num;
     statusBadge[0].addEventListener('click',function count(){
      num = Number(statusBadge[0].innerHTML);
